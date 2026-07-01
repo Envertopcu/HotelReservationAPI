6f89de269c470cf43a8edf566fda5bf1350ada3f
Enver Topcu <enver.topcu384@gmail.com>
2026-07-01 22:05:35 +0300

    feat: add Customer and Reservation models

--- HotelReservationAPI/Models/Customer.cs
+++ HotelReservationAPI/Models/Customer.cs
@@ -1,6 +1,27 @@
-﻿namespace HotelReservationAPI.Models
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationAPI.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ad zorunludur.")]
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Soyad zorunludur.")]
+        [StringLength(50)]
+        public required string LastName { get; set; }
+
+        [Required(ErrorMessage = "E-posta adresi zorunludur.")]
+        [EmailAddress(ErrorMessage = "Geçerli bir e-posta adresi giriniz.")]
+        public required string Email { get; set; }
+
+        [Phone(ErrorMessage = "Geçerli bir telefon numarası giriniz.")]
+        public string? PhoneNumber { get; set; }
     }
 }
--- HotelReservationAPI/Models/Reservation.cs
+++ HotelReservationAPI/Models/Reservation.cs
@@ -1,6 +1,34 @@
-﻿namespace HotelReservationAPI.Models
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelReservationAPI.Models
 {
     public class Reservation
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Müşteri bilgisi zorunludur.")]
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer? Customer { get; set; }
+
+        [Required(ErrorMessage = "Oda bilgisi zorunludur.")]
+        public int RoomId { get; set; }
+
+        [ForeignKey("RoomId")]
+        public Room? Room { get; set; }
+
+        [Required(ErrorMessage = "Giriş tarihi zorunludur.")]
+        public DateTime CheckInDate { get; set; }
+
+        [Required(ErrorMessage = "Çıkış tarihi zorunludur.")]
+        public DateTime CheckOutDate { get; set; }
+
+        public decimal TotalPrice { get; set; }
     }
 }
