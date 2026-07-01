24cef60fee2591b07af1707af5b7d221c8b839b0
Enver Topcu <enver.topcu384@gmail.com>
2026-07-01 22:00:41 +0300

    feat: add Customer and Reservation models

---
+++ HotelReservationAPI/Models/Reservation.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelReservationAPI.Models
+{
+    public class Reservation
+    {
+    }
+}
--- HotelReservationAPI/Models/Room.cs
+++ HotelReservationAPI/Models/Room.cs
@@ -1,6 +1,22 @@
-﻿namespace HotelReservationAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelReservationAPI.Models
 {
     public class Room
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Oda numarası zorunludur.")]
+        [StringLength(10)]
+        public required string RoomNumber { get; set; }
+
+        [Required(ErrorMessage = "Kapasite bilgisi zorunludur.")]
+        [Range(1, 10, ErrorMessage = "Kapasite 1 ile 10 arasında olmalıdır.")]
+        public int Capacity { get; set; }
+
+        [Required(ErrorMessage = "Gecelik fiyat zorunludur.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Geçerli bir fiyat giriniz.")]
+        public decimal PricePerNight { get; set; }
     }
 }
